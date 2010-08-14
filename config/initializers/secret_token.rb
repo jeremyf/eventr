6b5e1271e1d7218d1de399e2bca918e868c0a530
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2010-08-13 20:57:49 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Eventr::Application.config.secret_token = 'a64396306d1be083ebf7f6b17b5f7c96377264d1ea0e38dac74ce0f0d51c9cd5cb4f6edd78413bf04f127f7c4603037c153390ec9e454cbf776cd545347effb8'
