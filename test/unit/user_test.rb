fade72c6584e44056878c4150f7d17e9204092d6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2010-08-26 14:56:09 -0400

    Updated user test to replace deprecated shoulda macros

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  should_have_db_column :persistence_token
-  should_validate_uniqueness_of :login
-  should_validate_uniqueness_of :email
+  should have_db_column :persistence_token
+  should validate_uniqueness_of :login
+  should validate_uniqueness_of :email
 end
