4e51bbf2cdc2061215e819044002d61a8ed254b6
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-05-17 15:23:50 -0400

    Updated Event to refactor code placement.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,11 @@
 class Event < ActiveRecord::Base
   include ScheduleAttributes
-  validates_presence_of :name
-  has_many :occurrences, :dependent => :destroy
   attr_accessor :schedule_yaml
 
-  before_create :generate_occurrences
+  validates_presence_of :name
 
+  has_many :occurrences, :dependent => :destroy
+  before_create :generate_occurrences
   protected
   def generate_occurrences
     schedule.occurrences(:all).each do |occurrence|
