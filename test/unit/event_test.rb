14b1d539e31d3781eabfc2f130c57177a85bea65
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-05-17 12:00:04 -0400

    Updated Event test

--- test/unit/event_test.rb
+++ test/unit/event_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
   should have_db_column :name
-  should have_db_column :start_at
-  should have_db_column :end_at
   should validate_presence_of :name
-  should have_many :categorizations
-  should have_many :categories
+
+  should 'respond to .schedule' do
+    assert_respond_to Event.new, :schedule
+  end
 end
