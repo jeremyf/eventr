aec429dbfa38d46be1355017c4fee6a85f396a5d
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2011-05-18 11:11:02 -0400

    Update Occurrence concerning end_at both before and after save.

--- spec/models/occurrence_spec.rb
+++ spec/models/occurrence_spec.rb
@@ -8,18 +8,28 @@ describe Occurrence do
   it { should_not accept_values_for(:event_id, nil)}
   it { should accept_values_for(:end_at, nil)}
 
-  describe "#create" do
-    it 'without duration generates nil end_at' do
+  describe "#end_at" do
+    it 'without duration will be nil' do
       subject.start_at = Time.now
+      subject.end_at.should be_nil
+
+      subject.save!
+      subject.reload
+
       subject.end_at.should be_nil
     end
 
-    it 'with duration generates end_at' do
+    it 'with duration will be calculated from start date' do
       start_at = Time.now
       duration = 60*60
       subject.start_at = start_at
       subject.duration = duration
-      subject.end_at.should == (start_at + duration)
+      subject.end_at.utc.to_time.should == (start_at + duration).utc.to_time
+
+      subject.save!
+      subject.reload
+
+      subject.end_at.utc.to_s.should == (start_at + duration).utc.to_s
     end
   end
 end
